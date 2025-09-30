94228cad07911795f7f211dfbda733d4bd4bb7ce
Sfever <yanyoutong@outlook.com>
2025-09-30 22:35:06 +0800

    Add Developer Portal with login functionality and styling

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,11 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
     navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    }
 });
